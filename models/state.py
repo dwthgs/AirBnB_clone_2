0ee28f98c448c1bc54b539402a773825d25ba017
dev <dev@devs.local>
2021-08-10 10:26:21 +0300

    -

--- models/state.py
+++ models/state.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
-import sqlalchemy
-import os
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-    @property
-    def cities(self):
-        """ getter attribute cities that returns
-        list of City instances with state_id equals
-        to the current State.id """
-        self.state_id = State.id
-        li = []
-        all_city = models.storage.all(city)
-        for val in all_city.values():
-            if val.state_id == self.id:
-                li.append(val)
-        return li
+    name = ""
