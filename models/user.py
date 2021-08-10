1e8da8b3bd9698ce39819c09d0cb3cf4e3ce8d37
dev <dev@devs.local>
2021-08-10 12:52:01 +0300

    -

--- models/user.py
+++ models/user.py
@@ -9,20 +9,20 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = 'users'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-        places = relationship(
-            'Place', back_populates='user',
-            cascade='all, delete, delete-orphan')
-        reviews = relationship(
-            'Review', back_populates='user',
-            cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    places = relationship(
+        'Place', back_populates='user',
+        cascade='all, delete, delete-orphan')
+    reviews = relationship(
+        'Review', back_populates='user',
+        cascade='all, delete, delete-orphan')
+    # else:
+    #     email = ""
+    #     password = ""
+    #     first_name = ""
+    #     last_name = ""
