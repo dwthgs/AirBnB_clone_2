2dbbdc0dd3d5a14e7cd952b6a97d2959ede1d762
dev <dev@devs.local>
2021-08-10 13:24:15 +0300

    -

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- create database n grant permission
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE DATABASE IF NOT EXISTS performance_schema;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema .* TO 'hbnb_test'@'localhost';
