ea49ba7adc32ee5a1c9ab241d27a0a6ae655c93f
dev <dev@devs.local>
2021-08-10 12:23:15 +0300

    -

--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ from models.city import City, Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
