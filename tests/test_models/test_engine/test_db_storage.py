83f17ed40affefcc41d90ad9cd78ba0b9131638b
dev <dev@devs.local>
2021-08-10 13:25:49 +0300

    -

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,7 +2,6 @@
 """ Unittests for db_storage """
 import unittest
 import os
-import pep8
 import MySQLdb
 from models.base_model import BaseModel
 from models.user import User
@@ -18,13 +17,12 @@ import os
 class TestDBStorage(unittest.TestCase):
     """Tests for SQLAlchemy storage method."""
 
-
     db = MySQLdb.connect(host="localhost", user='hbnb_test',
-                             passwd='hbnb_test_pwd', db='hbnb_test_db')
+                         passwd='hbnb_test_pwd', db='hbnb_test_db')
     cur = db.cursor()
     os.environ['HBNB_MYSQL_USER'] = 'hbnb_test'
     os.environ['HBNB_MYSQL_PWD'] = 'hbnb_test_pwd'
-    os.environ['HBNB_MYSQL_HOST'] = 'localhost'
+    os.environ['HBNB_MYSQL_HOST'] = '127.0.0.1'
     os.environ['HBNB_MYSQL_DB'] = 'hbnb_test_db'
     os.environ['HBNB_TYPE_STORAGE'] = 'db'
     os.environ['HBNB_ENV'] = 'test'
@@ -37,12 +35,6 @@ class TestDBStorage(unittest.TestCase):
         cls.user.last_name = "KittyCat"
         cls.user.email = "murderface@bird.com"
         cls.storage = DBStorage()
-
-    def test_pep8_dbs(self):
-        """tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_db_basic(self):
         pass
