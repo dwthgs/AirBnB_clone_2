650380174ac1eb98b6a6ef3cd05897dbb5e653bf
dev <dev@devs.local>
2021-08-10 13:01:59 +0300

    -

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the city class"""
 
-import os
+# import os
 from models.base_model import BaseModel, Base, Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -15,11 +15,11 @@ class City(BaseModel, Base):
 
     __tablename__ = 'cities'
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        state = relationship(
-            'State', back_populates='cities')
-    else:
-        name = ""
-        state_id = ""
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state = relationship(
+        'State', back_populates='cities')
+    # else:
+    #     name = ""
+    #     state_id = ""
